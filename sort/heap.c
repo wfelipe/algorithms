01eaf49768b635da9618b74265a97b4c18811709
Wilson Felipe <wfelipe@gmail.com>
2009-10-03 00:08:22 -0300

    - small bug fix, we don't have to do a build max heap of size, size -1   is enough

--- sort/heap.c
+++ sort/heap.c
@@ -30,7 +30,7 @@ void heap_max_heapify (int *v, int size, int i)
 void heap_build_max_heap (int *v, int size)
 {
 	int i;
-	for (i = size/2; i > 0; i--)
+	for (i = (size -1)/2; i > 0; i--)
 		heap_max_heapify (v, size, i);
 }
 
